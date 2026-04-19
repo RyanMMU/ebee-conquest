46d4f9683b93c5dfdeda82756038fa94b64ae40e
BenedictChinJunEu <benedictcje@gmail.com>
2026-04-19 11:52:10 +0800

    updated some things

--- ingame_ui.py
+++ ingame_ui.py
@@ -3,7 +3,7 @@ import sys
 
 
 
-class Panel:
+class panel:
     def __init__(self, x, y, w, h, color=(40,40,40)):
         self.rect = pygame.Rect(x, y, w, h)
         self.color = color
@@ -12,29 +12,42 @@ class Panel:
         pygame.draw.rect(surface, self.color, self.rect)
         pygame.draw.rect(surface, (25,25,25), self.rect, 1)
 
-class Sidebar:
+class sidebar:
+
+
     def __init__(self, x, y, w, h):
-        self.rect = pygame.Rect(x, y, w, h)
-        self.items = []
+            self.rect = pygame.Rect(x, y, w, h)
+            self.items = []
+
 
     def word(self, name):
         self.items.append(name)
 
-
     def draw(self, surface, font):
         mouse = pygame.mouse.get_pos()
 
-        pygame.draw.rect(surface,(50,50,50), self.rect)
-        
+    
+
+        pygame.draw.rect(surface, (50, 50, 50), self.rect)
         
         for i, item in enumerate(self.items):
-                text_rect = font.render(item, False, (128, 128, 128)).get_rect(topleft=(self.rect.x + 20, self.rect.y + 60 + (i * 40)))
-                color = (255, 255, 255) if text_rect.collidepoint(mouse) else (128, 128, 128)
-                text = font.render(item, False, color)
-               
-                surface.blit(text, (self.rect.x + 20, self.rect.y + 60 + (i * 40)))
+          
+            x = self.rect.x + 10
+            y = self.rect.y + 60 + (i * 50)
+            w = self.rect.width - 20
+            h = 40
 
+            rect = pygame.Rect(x, y, w, h)
 
+            if rect.collidepoint(mouse):
+                color = (0, 200, 0)
+            else:
+                color = (30, 30, 30)
+
+            pygame.draw.rect(surface, color, rect)
+
+            text = font.render(item, True, (255, 255, 255))
+            surface.blit(text, (x + 10, y + 10))
 
 class page:
     def __init__(self):
@@ -44,14 +57,14 @@ class page:
         self.title_font = pygame.font.SysFont('Verdana', 16,bold= True)
         self.font = pygame.font.SysFont('Verdana', 14)
         
-        self.sidebar = Sidebar(0, 0, 250, 720)
+        self.sidebar = sidebar(0, 0, 250, 720)
         self.sidebar.word('URGENT')
         self.sidebar.word('LOGISTICS')
         self.sidebar.word('COMBAT')
         self.sidebar.word('INTEL')
-        self.topbar = Panel(0, 0, 1280, 50, (35,35,35))
-        self.rightbar = Panel(1030, 50, 250, 620, (0,0,0))
-        self.bottombar = Panel(0, 670, 1280, 50, (35,35,35))
+        self.topbar = panel(0, 0, 1280, 50, (35,35,35))
+        self.rightbar = panel(1030, 50, 250, 620, (0,0,0))
+        self.bottombar = panel(0, 670, 1280, 50, (35,35,35))
 
     def run(self):
         while True:
@@ -72,7 +85,7 @@ class page:
             
             pygame.display.flip()
           
-    pygame.quit() 
+   
 if __name__ == '__main__':
     page().run()
 
