c04cf36f18f9d96050200604fbb834b62136bd99
BenedictChinJunEu <benedictcje@gmail.com>
2026-04-26 19:07:01 +0800

    updated the game fullscreen

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,13 +1,2 @@
-import sys
-from pathlib import Path
-
-_project_root = Path(__file__).resolve().parent.parent #ebee conquest root
-if str(_project_root) not in sys.path:
-    sys.path.insert(0, str(_project_root))
-
-from engine.api import EbeeEngine
-from engine.events import EngineEventType, EventBus
-
-__all__ = ["EbeeEngine", "EngineEventType", "EventBus"]
 
 
